59a5f5304236a55c08ef9180f07b0d6b187b5665
Babybab <316067260@qq.com>
2019-03-15 14:04:30 +0800

    测试提交

---
+++ openlibrary/src/main/java/com/example/openlibrary/ToastUtils.java
@@ -0,0 +1,13 @@
+package com.example.openlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtils {
+    public static void Toast(Context context){
+        Toast.makeText(context, "测试", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+}
